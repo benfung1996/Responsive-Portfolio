a9866f49cf184cfa5bb25f8ff2a7d3a8dd969e93
Ben Fung <benfung1996@gmail.com>
2020-07-15 12:22:54 -0700

    Updated About page

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
     */
-    (function ($) {
+(function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
             location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
+            this.pathname.replace(/^\//, "") &&
             location.hostname == this.hostname
         ) {
             var target = $(this.hash);
@@ -40,6 +40,48 @@
         target: "#mainNav",
         offset: 74,
     });
+
+    // mailto
+
+    // Add class to mailto link
+    // Needed to separate the disabling of the default action AND copy email to clipboard
+    $('a[href^=mailto]').addClass('mailto-link');
+
+    var mailto = $('.mailto-link');
+    var messageCopy = 'Click to copy email address';
+    var messageSuccess = 'Email address copied to clipboard';
+
+    mailto.append('<span class="mailto-message"></span>');
+    $('.mailto-message').append(messageCopy);
+
+    // Disable default action (opening your email client. yuk.)
+    $('a[href^=mailto]').click(function () {
+        return false;
+    })
+
+    // On click, get href and remove 'mailto:'
+    // Store email address in a variable.
+    mailto.click(function () {
+        var href = $(this).attr('href');
+        var email = href.replace('mailto:', '');
+        copyToClipboard(email);
+        $('.mailto-message').empty().append(messageSuccess);
+        setTimeout(function () {
+            $('.mailto-message').empty().append(messageCopy);
+        }, 2000);
+    });
+
+
+    // Grabbed this from Stack Overflow.
+    // Copies the email variable to clipboard
+    function copyToClipboard(text) {
+        var dummy = document.createElement("input");
+        document.body.appendChild(dummy);
+        dummy.setAttribute('value', text);
+        dummy.select();
+        document.execCommand('copy');
+        document.body.removeChild(dummy);
+    }
 
     // Collapse Navbar
     var navbarCollapse = function () {
